b7515d4ccd752a7eeb8591b46c762d6d33da2656
Juan david burgos oquendo <j.dburgos@hotmail.com>
2021-08-19 17:27:39 -0500

    feat: add responsive styles

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -12,7 +12,7 @@ import styles from './Job.module.scss';
 
 export const Job = props => {
   const tags = [props.job.role, props.job.level, ...props.job.languages];
-  const content = tags.map((tag, index) => <Chip key={index} text={tag} />);
+  const content = tags.map((tag, index) => <Chip key={index} className={styles['job__tag']} text={tag} />);
 
   return (
     <Card className={styles['job']}>
--- src/components/UI/Chip/Chip.js
+++ src/components/UI/Chip/Chip.js
@@ -5,8 +5,14 @@ import PropTypes from 'prop-types';
 /** Styles **/
 import styles from './Chip.module.scss';
 
-export const Chip = props => <span className={styles.chip}>{props.text}</span>;
+export const Chip = props => {
+  const className = props.className ? props.className : '';
+  const classes = `${styles.chip} ${className} ${className}`.trim();
+
+  return <span className={classes}>{props.text}</span>;
+};
 
 Chip.propTypes = {
   text: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
